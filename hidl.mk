a108488efdbc85b54d2bcd674edd43dfb92ca8a3
Bruno Martins <bgcngm@gmail.com>
2018-02-07 00:41:09 +0000

    Zuk: fix Offline Charging
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794

--- hidl.mk
+++ hidl.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
