0428e73e59a70735468c7103862203b5ef5cb667
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-06-26 17:13:33 +0530

    Zuk: Add telephony-ext

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_COPY_FILES += \
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    telephony-ext \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
