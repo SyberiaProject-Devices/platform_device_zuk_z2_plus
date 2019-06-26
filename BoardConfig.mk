a292993ed3be10ffae1e8216c945868c52ced6ea
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-06-26 16:16:35 +0530

    Zuk: use Clang as ToolChain for building kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,16 +54,17 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/zuk/msm8996
-TARGET_KERNEL_CONFIG := z2_plus_defconfig
+TARGET_KERNEL_CONFIG := statix_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/kanishk/xtnd/prebuilts/gcc/linux-x86/aarch64/gcc9/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_PATH := /home/kanishk/xtnd/prebuilts/gcc/linux-x86/aarch64/clang9/bin
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Webview defs
 #PREBUILT_WEBVIEW_VERSION := chromium
